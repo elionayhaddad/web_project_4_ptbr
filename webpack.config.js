1899db222f3f676c18642362ab23610a07877be2
unknown <naygomes833@gmail.com>
2023-09-06 18:51:29 -0300

    feat: instalar o clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -51,6 +51,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
 };
